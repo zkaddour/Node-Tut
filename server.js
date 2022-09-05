038899fb45eb2132992bb60fa4cda8738b92371d
Zakaria <zaki.kaddour@outlook.fr>
2022-09-05 21:51:12 +0200

    Requests and Responses

--- server.js
+++ server.js
@@ -1,7 +1,38 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log('request made');
+    console.log(req.url, req.method);
+
+    // set header content type
+    res.setHeader('Content-Type', 'text/html');
+
+    // figure out the requested path
+    let path = './views/';
+    switch(req.url){
+        case '/':
+            res.statusCode = 200;
+            path += 'index.html';
+            break;
+        case '/about':
+            res.statusCode = 200;
+            path += 'about.html';
+            break;
+        default:
+            res.statusCode = 404;
+            path += '404.html';
+            break;
+    }
+
+    // send an HTML file
+    fs.readFile(path, (err, data) => {
+        if(err){
+            console.log(err);
+            res.end();
+        } else{
+            res.end(data);
+        }
+    })
 });
 
 server.listen(3000, 'localhost', () => {
